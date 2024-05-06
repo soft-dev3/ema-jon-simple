6b2106cbb7a0e6517dd67d9cf503b716f57dd0d3
soft-dev3 <mdsaddamkhan320@gamil.com>
2024-05-06 17:11:32 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../images/Logo.svg';
+const Header = () => {
+    return (
+        <div>
+            <img src={logo} alt="" />
+        </div>
+    );
+};
+
+export default Header;
